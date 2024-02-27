b9df4a9b76d401f2bfb685a713272915a704ff02
pingmeyes <sanjai.s.error@gmail.com>
2024-02-28 03:58:55 +0530

    test

--- index.php
+++ index.php
@@ -238,7 +238,7 @@ body {
             echo '<td>';
     
             // Styled delete button
-            echo '<form action="delete.php" method="post" style="display:inline-block; margin-right: 5px;">';
+            echo '<form action="delet.php" method="post" style="display:inline-block; margin-right: 5px;">';
             echo '<input type="hidden" name="id" value="' . $row['id'] . '">';
             echo '<button type="submit" onclick="return confirm(\'Are you sure you want to delete this record?\');" style="background-color: #f44336; color: white; border: none; padding: 8px 12px; cursor: pointer; border-radius: 4px;">Delete</button>';
             echo '</form>';

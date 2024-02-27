1e02f7b4510c2a0b2efa554ad8c50f68575d7dd6
pingmeyes <sanjai.s.error@gmail.com>
2024-02-28 02:50:35 +0530

    test

--- index.php
+++ index.php
@@ -158,8 +158,7 @@ body {
     </select>
 
     <button type="submit">Submit</button>
-  </form>  
-  Hello
+  </form>
 <div><a href="empty.php" onclick="return confirm('Are you sure you want to delete all records?');">Delete All Records</a></div>
 <div class="dashboard-section">
     <h2>Dashboard</h2>

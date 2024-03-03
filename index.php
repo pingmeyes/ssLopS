ccd37e4e55e05c66d8f87f5dca8c1879816e53b8
pingmeyes <sanjai.s.error@gmail.com>
2024-03-03 20:21:23 +0530

    test

--- index.php
+++ index.php
@@ -215,31 +215,6 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
         <button type="submit">Submit</button>
     </form>
-    <div class="top-right-sectiohfgs">
-        <h2>Expiring Domains</h2>
-        <!-- Display expiring domains within the main content -->
-        <?php
-        // Fetch domains with expiry days less than 30
-        $sqlFetchExpiring = "SELECT * FROM ssl_details WHERE DaysLeftToExpire < 30";
-        $resultFetchExpiring = $conn->query($sqlFetchExpiring);
-
-        while ($rowExpiring = $resultFetchExpiring->fetch_assoc()) {
-            $expiryDays = $rowExpiring['DaysLeftToExpire'];
-            $expiryClass = '';
-            if ($expiryDays < 10) {
-                $expiryClass = 'expiry-box-red';
-            } elseif ($expiryDays < 20) {
-                $expiryClass = 'expiry-box-orange';
-            } elseif ($expiryDays < 30) {
-                $expiryClass = 'expiry-box-dark-yellow';
-            }
-
-            echo '<div class="expiry-box ' . $expiryClass . '">';
-            echo $rowExpiring['domainName'] . ' - Expires in ' . $expiryDays . ' days';
-            echo '</div>';
-        }
-        ?>
-    </div>
       </div>
     <div><a href="empty.php" onclick="return confirm('Are you sure you want to delete all records?');">Delete All Records</a></div>
 
@@ -323,7 +298,31 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
             ?>
             </tbody>
         </table>
-            
+        <div class="top-right-section">
+        <h2>Expiring Domains</h2>
+        <!-- Display expiring domains within the main content -->
+        <?php
+        // Fetch domains with expiry days less than 30
+        $sqlFetchExpiring = "SELECT * FROM ssl_details WHERE DaysLeftToExpire < 30";
+        $resultFetchExpiring = $conn->query($sqlFetchExpiring);
+
+        while ($rowExpiring = $resultFetchExpiring->fetch_assoc()) {
+            $expiryDays = $rowExpiring['DaysLeftToExpire'];
+            $expiryClass = '';
+            if ($expiryDays < 10) {
+                $expiryClass = 'expiry-box-red';
+            } elseif ($expiryDays < 20) {
+                $expiryClass = 'expiry-box-orange';
+            } elseif ($expiryDays < 30) {
+                $expiryClass = 'expiry-box-dark-yellow';
+            }
+
+            echo '<div class="expiry-box ' . $expiryClass . '">';
+            echo $rowExpiring['domainName'] . ' - Expires in ' . $expiryDays . ' days';
+            echo '</div>';
+        }
+        ?>
+    </div>    
 
       </div>
 </div>

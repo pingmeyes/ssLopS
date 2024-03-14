dadbd769d988022d443fc35dd0b2f43e2ae7b6b6
pingmeyes <sanjai.s.error@gmail.com>
2024-03-14 22:28:45 +0530

    delete fn to both table

--- empty.php
+++ empty.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 
 // Delete all records from ssl_details
 $sqlDeleteAll = "DELETE FROM ssl_details;";
-$sqlDeleteAll = "DELETE FROM manual_ssl_details;";
+$sqlDeleteAllman = "DELETE FROM manual_ssl_details;";
 
 
 if ($conn->query($sqlDeleteAll) === TRUE) {
